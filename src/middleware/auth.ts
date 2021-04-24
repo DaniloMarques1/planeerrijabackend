c1a441dd940638fbce70c86b6543b593585ec2a9
Danilo Marques <danilomarques20@hotmail.com>
2021-04-24 13:25:58 -0300

    Validando o id passado passado pelo token pelo middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,14 +2,25 @@ import {Request, Response, NextFunction} from 'express';
 import {GeneralError} from '../utils/generalError';
 import {Helper} from '../utils/helper';
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
-  const auth = <string>req.headers["authorization"]
-  console.log(auth);
-  if (!auth)
-    return res.status(401).json(new GeneralError("Missing token"));
-  const token = auth.split(" ")[1];
-  const employeeId = Helper.getPayload(token);
-  req.headers["id"] = employeeId;
+import {EmployeeRepository} from '../repository/employeeRepository';
 
-  return next();
+export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
+	try {
+		const auth = <string>req.headers["authorization"]
+		if (!auth)
+			return res.status(401).json(new GeneralError("Missing token"));
+		const token = auth.split(" ")[1];
+		const employeeId = Helper.getPayload(token);
+
+		const employee = await EmployeeRepository.findEmployeeById(Number(employeeId));
+		if (!employee) {
+			return res.status(401).json(new GeneralError("Invalid token"));
+		}
+
+		req.headers["id"] = employeeId;
+
+		return next();
+	}catch(e) {
+		return res.status(401).json(new GeneralError("Invalid token"));
+	}
 }
